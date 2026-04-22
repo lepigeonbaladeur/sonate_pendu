bd53ac6c6b8f0348d39e6eba6cfd83df4f4c7cc2
lepigeonbaladeur <damien.legentil@posteo.net>
2026-04-22 18:35:59 +0200

    Ajout du dossier templates et des fichiers html

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
