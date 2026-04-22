f6b107f57e5a4fff26c0bf376f2d1b1e60eae0aa
lepigeonbaladeur <damien.legentil@posteo.net>
2026-04-22 21:22:33 +0200

    ajout de fichiers et dossiers

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/play", methods=["POST"])
+def play():
+    if request.method == "POST":
+        speudo = request.form["pseudo"]
+    return render_template("play.html")
