2ce5765abf27ba32e8b2d697c6d1adec8579edd9
lepigeonbaladeur <damien.legentil@posteo.net>
2026-04-24 00:16:06 +0200

    Refonte total graphique + ajout de la scene de jeu

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import string
+import random
+
+
 
 app = Flask(__name__)
 
+app.secret_key = "a847v7Pr$PD8!rhsv!w8B6Jfrx*7wY422ne*"
 
 @app.route("/")
 def home():
@@ -10,6 +14,32 @@ def home():
 
 @app.route("/play", methods=["POST"])
 def play():
-    if request.method == "POST":
-        speudo = request.form["pseudo"]
-    return render_template("play.html", clavier = string.ascii_uppercase)
+    session['vies'] = 5
+    session['lettres_trouvees']= []
+    fichier = r"./dictionnaire.txt"
+    lignes = []
+    f = open(fichier, "r", encoding="utf-8")
+    for ligne in f:
+        ligne = ligne.rstrip().split(";")[0]
+        lignes.append(ligne) 
+    f.close()
+    mot=random.choice(lignes)
+    pseudo = request.form["pseudo"]
+    session['mot'] = mot
+    session['pseudo'] = pseudo
+    return render_template("play.html", clavier = string.ascii_uppercase,pseudo = pseudo, mot=mot, lettres_trouvees=session['lettres_trouvees'])
+
+
+
+@app.route('/guess', methods=["POST"])
+def guess():
+    pseudo = session["pseudo"]
+    lettre = request.form["lettre"]
+    mot = session['mot'].upper()
+    if lettre in mot:
+        session['lettres_trouvees'].append(lettre)
+        session.modified = True
+    else:
+        session['vies'] -= 1
+    return render_template("play.html", clavier = string.ascii_uppercase,pseudo = pseudo, mot=mot, lettres_trouvees=session['lettres_trouvees'])
+
